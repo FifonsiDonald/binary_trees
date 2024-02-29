ae926c67e45bfc969e1a953fd375d4ba5b279bbe
FifonsiDonald <dfifonsi@st.ug.edu.gh>
2024-02-29 13:20:40 -0800

    task 2 complete

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -4,6 +4,12 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	binary_tree_t *node;
 
 	node = binary_tree_node(parent, value);
+	
+	if (parent->left != NULL)
+	{
+		node->left = parent->left;
+		parent->left->parent = node;
+	}	
 	parent->left = node;
 
 	return (node);
