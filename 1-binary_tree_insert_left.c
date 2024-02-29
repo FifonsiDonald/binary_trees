b2ccb8a349698b655363056ff17fac447ef8d42a
FifonsiDonald <dfifonsi@st.ug.edu.gh>
2024-02-29 12:17:12 -0800

    task 02 test

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,10 @@
+#include "binary_trees.h"
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *node;
+
+	node = binary_tree_node(parent, value);
+	parent->left = node;
+
+	return (node);
+}
