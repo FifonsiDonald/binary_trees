f5bcff45dc2cea9cd99ea7a049492366c73fbd30
FifonsiDonald <dfifonsi@st.ug.edu.gh>
2024-02-29 13:40:46 -0800

    task 01 fix and task 02 complete

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,15 +1,26 @@
 #include "binary_trees.h"
+/**
+ * binary_tree_insert_left - Inserts a node as a left-child
+ *
+ * @parent: parent  of the node.
+ * @value: The value.
+ *
+ * Return: left node unless error.
+ */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node;
 
 	node = binary_tree_node(parent, value);
-	
+
+	if (parent == NULL)
+		return (NULL);
+
 	if (parent->left != NULL)
 	{
 		node->left = parent->left;
 		parent->left->parent = node;
-	}	
+	}
 	parent->left = node;
 
 	return (node);
---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,27 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_insert_right - Inserts a node as a right-child
+ *
+ * @parent: parent  of the node.
+ * @value: The value.
+ *
+ * Return: right node unless error.
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+        binary_tree_t *node;
+
+        node = binary_tree_node(parent, value);
+
+        if (parent == NULL)
+                return (NULL);
+
+        if (parent->right != NULL)
+        {
+                node->right = parent->right;
+                parent->right->parent = node;
+        }
+        parent->right = node;
+
+        return (node);
+}
